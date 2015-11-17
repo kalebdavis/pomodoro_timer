23b0be818b0835c305e11e8093165921b4f4a995
Kaleb Davis <kalebdavisgithub@gmail.com>
2015-11-16 21:20:10 -0500

    add logging functionality and sound to play

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,32 +1,38 @@
-puts 'Is this a work (w) or a rest (r) block?'
-b = gets.chomp
-limit = 5
-label = ''
-if b == 'w'
-	limit = 25
-	puts 'What are you working on?'
-	label = gets.chomp
-	puts 'Work hard!'
-end
-system('clear')
-# Timing using countdown
-clock = Time.at(limit * 60)
-while clock != Time.at(0)
-	clock -= 1
-	sleep(1)
-	system 'clear'
-	puts clock.strftime "%M:%S"
-end
+log_file = 'log.txt'
+while true do
+    puts 'Is this a work (w) or a rest (r) block?'
+    b = gets.chomp
+    limit = 5
+    label = ''
+    if b == 'w'
+        limit = 25
+        puts 'What are you working on?'
+        label = gets.chomp
+        puts 'Work hard!'
+    end
+    system('clear')
+    # Timing using countdown
+    clock = Time.at(limit * 60)
+    while clock != Time.at(0)
+        clock -= 1
+        sleep(1)
+        system 'clear'
+        puts clock.strftime "%M:%S"
+    end
 
-# Timing using actual time
-# start = Time.now
-# total_time = Time.now - start
-# while(total_time < (limit * 60))
-# 	sleep(1)
-# 	if total_time.to_i % 60 == 0
-# 		puts "#{total_time.to_i / 60} minutes completed"
-# 	end
-# end
-system("aplay /usr/share/sounds/alsa/beep.wav")
-puts 'You finished your pomodoro!'
-puts "You worked on: #{label}" unless label.empty?
+    # Timing using actual time
+    # start = Time.now
+    # total_time = Time.now - start
+    # while(total_time < (limit * 60))
+    # 	sleep(1)
+    # 	if total_time.to_i % 60 == 0
+    # 		puts "#{total_time.to_i / 60} minutes completed"
+    # 	end
+    # end
+    system("afplay beep-05.wav")
+    puts 'You finished your pomodoro!'
+    puts "You worked on: #{label}" unless label.empty?
+    File.open(log_file, 'a') do |f|
+        f.write("#{Time.now.strftime("%m-%d-%y, %H:%M")}\n\t#{label}")
+    end
+end
